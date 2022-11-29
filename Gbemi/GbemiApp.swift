f07e1180b8fee40247c15e8e0d50a6d0f30b60ad
Waheed Afolabi <waheed.remote247@gmail.com>
2022-11-29 18:25:34 +0100

    Initial Commit

---
+++ Gbemi/GbemiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GbemiApp.swift
+//  Gbemi
+//
+//  Created by waheedCodes on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GbemiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
